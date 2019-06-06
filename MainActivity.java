982e5e4a274322f9744651c05ef92ab3ff44f355
Avinash Kumar <44208272+avinashmallik62@users.noreply.github.com>
2019-06-06 16:42:59 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,33 @@
+package com.avinash.currencyconverter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void convert(View view){
+
+        EditText dollarAmountEditText = (EditText)findViewById(R.id.dollarAmountEditText);
+        
+        Double dollarAmountDouble = Double.parseDouble(dollarAmountEditText.getText().toString());
+        
+        Double RupeeAmount = dollarAmountDouble * 69.29 ;
+        
+        Toast.makeText(MainActivity.this,"₹" + RupeeAmount.toString(), Toast.LENGTH_SHORT).show();
+        
+        Log.i("amount",dollarAmountEditText.getText().toString());
+
+
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
